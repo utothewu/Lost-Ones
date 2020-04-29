e93e816800723aa7d9af9b9c7d188633efa25c2c
gerdescedric <gerdescedric@penguin>
2020-04-29 16:52:28 -0600

    mostly a webhook test

---
+++ engine.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* I'm going to try mocking up the engine */
+
+//loads needed assets
+function preload() {
+
+}
+
+//initializes
+function setup() {
+
+}
+
+//runs at roughly 60fps
+function draw() {
+
+}
+
+//runs everything needed for one game tick
+function tick() {
+
+}
+
+/* I'm going to need some way of syncronizing the game across clients.
+I need some method for collision detection. Every tile will have a class
+of attributes: x, y, type, and the like. I need a protocol for the order
+in which tiles are drawn. */
