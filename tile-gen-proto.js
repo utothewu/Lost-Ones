0b8618cae49259cae8ae141a87b2a55aee30acdf
RandomlyTree <33067456+RandomlyTree@users.noreply.github.com>
2020-04-28 21:29:21 -0600

    Add files via upload

--- tile-gen-proto.js
+++ tile-gen-proto.js
@@ -3,15 +3,27 @@
 let sheet;
 let columns = 10;
 let rows = 10;
-let tDim = 16
+let tDim = 32;
 
+class tile {
+  draw() {
+    if(this.type === "flowers"){
+      text(1, this.x, this.y);
+    } else if (this.type === "grass") {
+      text(2, this.x, this.y);
+    } else if (this.type === "tallGrass") {
+      text(3, this.x, this.y);
+    } else if (this.type === "bush") {
+      text(4, this.x, this.y);
+    } else if (this.type === "tree") {
+      text(5, this.x, this.y);
+    }
+  }
+}
 
 function setup() {
-  createCanvas(320, 320);
-  tileGen(0, 0);
-  tileGen(160, 0);
-  tileGen(0, 160);
-  tileGen(160, 160);
+  createCanvas(640, 640);
+  tileGen(0, 0)
 }
 
 function draw() {
@@ -22,29 +34,51 @@ function init() {
 
 }
 
+//function for generating a chunk
 function tileGen(startX, startY) {
+  //initialise arrays
   sheet = new Array(columns);
   for (let i = 0; i < columns; i++) {
     sheet[i] = new Array(rows);
   }
-
-  for ( let i = 0; i < columns; i++) {
-    for ( let j = 0; j < rows; j++) {
-      let x = startX + tDim * i;
-      let y = startY + tDim * j;
-
-      noiseDetail(4, 0.35);
-      let RGB = Math.round(Math.abs(255 * noise(x, y)));
-      fill(RGB);
-
-      if(RGB > 179) fill("#0000ff")
-      if(RGB < 180) fill("#a4c903")
-      if(RGB < 100) fill("#80b405");
-      if(RGB < 60) fill("#008f00");
-      if(RGB < 45) fill("#006f5f");
-
-      stroke(0);
-      square(x, y, tDim);
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      sheet[i][j] = new tile;
     }
   }
+
+  //targets each block I don't know if I can use for in and return the right order
+  for ( let i = 0; i < columns; i++) {
+    for ( let j = 0; j < rows; j++) {
+      //perlin noise
+      let xOff = i * 0.5;
+      let yOff = j * 0.5;
+      noiseDetail(3, 0.35);
+      let noiseOut = Math.round(Math.abs(255 * noise(xOff, yOff)));
+
+      //type assignment
+      if(noiseOut > 179 && noiseOut < 255) sheet[i][j].type = "flowers";
+      if(noiseOut > 99 && noiseOut < 180) sheet[i][j].type = "grass";
+      if(noiseOut > 79 && noiseOut < 100) sheet[i][j].type = "tallGrass";
+      if(noiseOut > 54 && noiseOut < 80) sheet[i][j].type = "bush";
+      if(noiseOut > 0 && noiseOut < 55) sheet[i][j].type = "tree";
+
+      console.log(sheet[i][j]);
+
+      let x = (i * tDim) + startX;
+      let y = (j * tDim) + startY;
+      sheet[i][j].x = x;
+      sheet[i][j].y = y;
+
+      fill(noiseOut);
+      textSize(20);
+      sheet[i][j].draw()
+    }
+  }
+}
+
+function tileDraw(startX, startY) {
+
+
+
 }
